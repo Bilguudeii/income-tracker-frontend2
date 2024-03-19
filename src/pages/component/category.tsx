944d49df652a17c5d00d86e06c429a8dbec1c1b1
Билгүүдэй Бүдшир <HP231031@G303-CC0131.local>
2024-03-19 18:56:55 +0800

    aaaa

---
+++ src/pages/component/category.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Checked } from "../Icons/checked";
+import { Leading } from "../Icons/leading";
+
+function Category() {
+  return (
+    <div>
+      <Checked />
+      <h3>food</h3>
+      <Leading />
+    </div>
+  );
+}
+
+export default Category;
--- src/pages/records/index.tsx
+++ src/pages/records/index.tsx
@@ -1,28 +1,61 @@
 import { GeldIconLogo } from "../Icons/GeldLogo";
 import { Plus } from "../Icons/Plus";
+import Category from "../component/category";
+import { BluePlus } from "../Icons/BluePlus";
 
 const Records = () => {
-    return (
-        <div className="MMAIN">
-            <div className="MAIIIN">
-                <div className="navbar">
-                    <div className="topmain">
-                        <GeldIconLogo />
-                        <h2 className="dash">Dashboard</h2>
-                        <div className="records">Records</div>
-                    </div>
-                    <div className="top2main">
-                        <div>
-                            <button className="buttonRec">
-                                <Plus /> <h3 className="buttonRecc">Record</h3>
-                            </button>
-                        </div>
-                        <img className="prof" src="prof.jpeg" />
-                    </div>
-                </div>
+  return (
+    <div className="MMAIN">
+      <div className="MAIIIN">
+        <div className="navbar">
+          <div className="topmain">
+            <GeldIconLogo />
+            <h2 className="dash">Dashboard</h2>
+            <div className="records">Records</div>
+          </div>
+          <div className="top2main">
+            <div>
+              <button className="buttonRec">
+                <Plus /> <h3 className="buttonRecc">Record</h3>
+              </button>
             </div>
+            <img className="prof" src="prof.jpeg" />
+          </div>
         </div>
-    )
-}
+        <div>
+          <div className="ttRecords">
+            <div>
+              <h2 className="tRecords">Records</h2>
+              <button className="ntb">
+                <Plus /> Add
+              </button>
+              <input className="ttInput" placeholder="Search" />
+              <h3>Types</h3>
+              <div>
+                <input className="ttCheckBox" type="checkbox" /> <h3>All</h3>
+              </div>
+              <div>
+                <input className="ttCheckBox" type="checkbox" /> <h3>Income</h3>
+              </div>
+              <div>
+                <input className="ttCheckBox" type="checkbox" /> <h3>Expense</h3>
+              </div>
+            </div>
+            <div>
+              <h2>Category</h2>
+              {[...Array(10)].map(() => (
+                <Category/>
+              ))}
+            </div>
+            <div>
+                <BluePlus/> <h3>Add Category</h3>
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Records
+export default Records;
